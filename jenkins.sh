8f47b01038eaf3cf243ccbf8bdaced8e22615896
mabhinay <abhinay.mahajan9@gmail.com>
2021-02-25 11:00:32 +0530

    jenkins install script

---
+++ jenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum -y install java-1.8.0
+sudo yum -y install java-1.8.0-devel
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum -y install jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
