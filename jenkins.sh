05c2e10180f7a8d88912f72cd5852f4a5b0cfaad
mabhinay <abhinay.mahajan9@gmail.com>
2021-02-25 05:47:42 +0000

    modified jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+yum -y install wget
+yum -y install git
 sudo yum -y install java-1.8.0
 sudo yum -y install java-1.8.0-devel
+yum -y install maven
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum -y install jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-yum -y install maven
-yum -y install git
-yum -y install wget
--- maven.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-yum -y install java-1.8.0 java-1.8.0-devel
-yum -y install maven
