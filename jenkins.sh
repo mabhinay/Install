a1857b1c67d126abd9b912a86d28624ab6809ab6
mabhinay <abhinay.mahajan9@gmail.com>
2022-03-01 11:23:48 +0530

    added line repository that provides 'daemonize'

--- jenkins.sh
+++ jenkins.sh
@@ -6,6 +6,7 @@ sudo yum -y install java-1.8.0-devel
 sudo yum -y install maven
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install epel-release -y
 sudo yum -y install jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
