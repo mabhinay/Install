995188f210f52eb4d2414f0d04926238df94368f
mabhinay <abhinay.mahajan9@gmail.com>
2022-03-01 21:04:45 +0530

    line removed

--- jenkins.sh
+++ jenkins.sh
@@ -8,7 +8,6 @@ yum install -y ca-certificates
 sudo yum install epel-release -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum install epel-release -y
 sudo yum -y install jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
