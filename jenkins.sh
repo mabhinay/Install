c7aecba473d141b1037c9bbe08afee90c86943c4
mabhinay <abhinay.mahajan9@gmail.com>
2022-03-02 10:06:10 +0530

    sudo added

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ sudo yum -y install git
 sudo yum -y install java-1.8.0
 sudo yum -y install java-1.8.0-devel
 sudo yum -y install maven
-yum install -y ca-certificates
+sudo yum install -y ca-certificates
 sudo yum install epel-release -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
